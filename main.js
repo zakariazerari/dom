d3a5477bccd70020dac5de37bad11a126463e767
zakariazerari <zakariazerari042@gmail.com>
2025-02-19 15:37:39 +0100

    dom1

--- main.js
+++ main.js
@@ -2,21 +2,34 @@ document.getElementById("form").addEventListener("submit", function(event){
 event.preventDefault();
 
 let email = document.getElementById("email").value;
-let pasword =  document.getElementById("password").value;
+let password =  document.getElementById("password").value;
 let message = document.getElementById("message");
-let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-if(!emailPattern.test(email)) {
-    message.style.color = "red";
-    message.textContent = "❌ البريد الإلكتروني غير صالح!";
-    return;
+if (email == "zakariazerari042@gmail.com" && password == "Raja1949@@"){
+    message.style.color = 'green';
+    message.textContent = "Valid email✅"
+    return
 }
-if(pasword.length < 6) {
-    message.style.color = "red";
-    message.textContent = "❌ البريد الإلكتروني غير صالح!";
-    return;
+else{
+    message.style.color = 'red';
+    message.textContent = "Invalid email❌"
+    return
 }
-message.style.color = "green";
-            message.textContent = "✅ البريد وكلمة المرور صحيحان!";
 
-});
+})
+// let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// if(!emailPattern.test(email)) {
+//     message.style.color = "red";
+//     message.textContent = "❌ البريد الإلكتروني غير صالح!";
+//     return;
+// }
+// if(pasword.length < 6) {
+//     message.style.color = "red";
+//     message.textContent = "❌ البريد الإلكتروني غير صالح!";
+//     return;
+// }
+// message.style.color = "green";
+//             message.textContent = "✅ البريد وكلمة المرور صحيحان!";
+
+// })
